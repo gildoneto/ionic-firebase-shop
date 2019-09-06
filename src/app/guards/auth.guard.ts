e61907c1180df1afe30b6c36af677c7362a2cc15
Gildo Neto <gildorama@gmail.com>
2019-09-06 19:25:18 -0300

    configurando o aut.guard que vai redirecionar o user pro login se não estiver logado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  /*Esse canActivate vai fazer com que ele retorne
+    pra página de login caso não estaja logado */
+  canActivate(): Promise<boolean> {
+    return new Promise(resolve => {
+      this.authService.getAuth().onAuthStateChanged(user => {
+        if (!user) this.router.navigate(['login']);
+
+        resolve(user ? true : false);
+      });
+    });
+  }
 }

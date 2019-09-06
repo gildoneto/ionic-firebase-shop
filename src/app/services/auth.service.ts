06d210e8754a539632504a4c51ccf999c54ecdc5
Gildo Neto <gildorama@gmail.com>
2019-09-06 18:52:11 -0300

    criando guards para segurança de rota

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(private afa: AngularFireAuth) { }
 
   login(user: User) {

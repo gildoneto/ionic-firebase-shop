e7f27ca40241552755651f2548cbaca943e0396e
Gildo Neto <gildorama@gmail.com>
2019-09-06 13:32:59 -0300

    adicionando a variável firebase em enviroment.ts com o objeto de configuração do projeto no firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC50OzSTeZn3VEaTZuw_pA3Y3SAN8f9sjs",
+    authDomain: "ionic-firebase-shop-c6277.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-shop-c6277.firebaseio.com",
+    projectId: "ionic-firebase-shop-c6277",
+    storageBucket: "",
+    messagingSenderId: "271095910056",
+    appId: "1:271095910056:web:7dd49c802fe8c4f85c3c9d"
+  }
 };
 
 /*
